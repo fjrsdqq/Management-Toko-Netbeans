4d11d91e74cad26f1958a4c5829acffe39367017
unknown <scarletnr5@gmail.com>
2025-05-23 13:17:27 +0700

    perbarui database

--- Database/db_penggilingan.sql
+++ Database/db_penggilingan.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2025 at 02:30 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 23 Bulan Mei 2025 pada 08.15
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Struktur dari tabel `admin`
 --
 
 CREATE TABLE `admin` (
@@ -36,7 +36,7 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `admin`
+-- Dumping data untuk tabel `admin`
 --
 
 INSERT INTO `admin` (`id_admins`, `nama`, `username`, `password`, `role`) VALUES
@@ -46,7 +46,7 @@ INSERT INTO `admin` (`id_admins`, `nama`, `username`, `password`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `barang`
+-- Struktur dari tabel `barang`
 --
 
 CREATE TABLE `barang` (
@@ -58,7 +58,7 @@ CREATE TABLE `barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `barang`
+-- Dumping data untuk tabel `barang`
 --
 
 INSERT INTO `barang` (`id_barang`, `nama_bahan`, `jenis_bahan`, `jumlah_stock`, `keterangan`) VALUES
@@ -68,7 +68,7 @@ INSERT INTO `barang` (`id_barang`, `nama_bahan`, `jenis_bahan`, `jumlah_stock`, 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `karyawan`
+-- Struktur dari tabel `karyawan`
 --
 
 CREATE TABLE `karyawan` (
@@ -81,7 +81,7 @@ CREATE TABLE `karyawan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `karyawan`
+-- Dumping data untuk tabel `karyawan`
 --
 
 INSERT INTO `karyawan` (`id_karyawan`, `nama_karyawan`, `jabatan`, `gaji`, `no_hp`, `alamat`) VALUES
@@ -90,28 +90,55 @@ INSERT INTO `karyawan` (`id_karyawan`, `nama_karyawan`, `jabatan`, `gaji`, `no_h
 -- --------------------------------------------------------
 
 --
--- Table structure for table `keuangan`
+-- Struktur dari tabel `keuangan`
 --
 
 CREATE TABLE `keuangan` (
   `id_keuangan` int(11) NOT NULL,
   `tanggal` date NOT NULL,
-  `tipe` enum('Masuk','Keluar') NOT NULL,
+  `tipe` enum('Pemasukan Harian','Pengeluaran Harian') NOT NULL,
   `keterangan` text DEFAULT NULL,
   `jumlah` decimal(15,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `keuangan`
+-- Dumping data untuk tabel `keuangan`
 --
 
 INSERT INTO `keuangan` (`id_keuangan`, `tanggal`, `tipe`, `keterangan`, `jumlah`) VALUES
-(1, '2025-04-28', 'Masuk', 'Penjualan Harian', 500000.00);
+(4, '2025-05-08', 'Pemasukan Harian', 'vfdcv', 21321.00),
+(5, '2025-05-08', '', 'pengluaran harian', 22222.00),
+(6, '2025-05-08', 'Pemasukan Harian', 'pengluaran harian', 111.00),
+(8, '2025-05-08', 'Pemasukan Harian', 'ff', 22.00),
+(9, '2025-05-16', 'Pemasukan Harian', 'vfdcv', 222.00),
+(11, '2025-05-17', 'Pemasukan Harian', 'pengluaran harian', 1234.00),
+(12, '2025-05-16', 'Pemasukan Harian', 'qwerty', 1234567890.00),
+(14, '2025-05-24', 'Pemasukan Harian', 'ancbcd', 0.00),
+(15, '2025-05-09', 'Pemasukan Harian', 'aaaaaaaa', 1234.00),
+(18, '2025-05-09', 'Pemasukan Harian', 'dfsdsfsd', 12332432433.00),
+(19, '2025-05-10', '', 'aaaaaaaaaaaaa', 32411111.00),
+(20, '2025-05-10', 'Pemasukan Harian', 'ddddddddd', 222222.00),
+(21, '2025-05-10', '', 'eeeee', 222222222.00),
+(22, '2025-05-17', '', 'aaaaasss', 1111122.00),
+(23, '2025-05-03', '', 'aaaa', 12333.00),
+(24, '2025-05-17', '', 'aqqqq', 11111111.00),
+(26, '2025-05-10', '', 'aaaaaaaa', 1111111.00),
+(27, '2025-05-10', 'Pemasukan Harian', '1111122', 11111111.00),
+(28, '2025-05-03', '', 'aaaaaaa', 11111111.00),
+(30, '2025-05-24', '', 'ada', 2000.00),
+(31, '2025-05-17', 'Pemasukan Harian', '11133', 0.00),
+(32, '2025-05-03', 'Pemasukan Harian', '224', 0.00),
+(33, '2025-05-03', '', 'ee', 11.00),
+(34, '2025-05-17', 'Pengeluaran Harian', 'ww', 3333.00),
+(35, '2025-05-10', 'Pemasukan Harian', 'ww', 222.00),
+(36, '2025-05-08', 'Pemasukan Harian', 'ff', 22.00),
+(37, '2025-05-17', 'Pengeluaran Harian', 'ww', 22.00),
+(38, '2025-05-14', 'Pemasukan Harian', 'bakso tahu', 2000000.00);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pelanggan`
+-- Struktur dari tabel `pelanggan`
 --
 
 CREATE TABLE `pelanggan` (
@@ -123,16 +150,17 @@ CREATE TABLE `pelanggan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `pelanggan`
+-- Dumping data untuk tabel `pelanggan`
 --
 
 INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `no_hp`, `alamat`, `perusahaan`) VALUES
-(2, 'kafe1', '12345', 'bogor', 'ceriamandiri');
+(2, 'kafe1', '12345', 'bogor', 'ceriamandiri'),
+(3, '3', '6757', 'hjgj', 'gkjh');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `supplier`
+-- Struktur dari tabel `supplier`
 --
 
 CREATE TABLE `supplier` (
@@ -144,7 +172,7 @@ CREATE TABLE `supplier` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `supplier`
+-- Dumping data untuk tabel `supplier`
 --
 
 INSERT INTO `supplier` (`id_supplier`, `nama_supplier`, `alamat`, `no_hp`, `email`) VALUES
@@ -153,7 +181,7 @@ INSERT INTO `supplier` (`id_supplier`, `nama_supplier`, `alamat`, `no_hp`, `emai
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaksi_pelanggan`
+-- Struktur dari tabel `transaksi_pelanggan`
 --
 
 CREATE TABLE `transaksi_pelanggan` (
@@ -169,101 +197,101 @@ CREATE TABLE `transaksi_pelanggan` (
 --
 
 --
--- Indexes for table `admin`
+-- Indeks untuk tabel `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id_admins`) USING BTREE,
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- Indexes for table `barang`
+-- Indeks untuk tabel `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
--- Indexes for table `karyawan`
+-- Indeks untuk tabel `karyawan`
 --
 ALTER TABLE `karyawan`
   ADD PRIMARY KEY (`id_karyawan`);
 
 --
--- Indexes for table `keuangan`
+-- Indeks untuk tabel `keuangan`
 --
 ALTER TABLE `keuangan`
   ADD PRIMARY KEY (`id_keuangan`);
 
 --
--- Indexes for table `pelanggan`
+-- Indeks untuk tabel `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`);
 
 --
--- Indexes for table `supplier`
+-- Indeks untuk tabel `supplier`
 --
 ALTER TABLE `supplier`
   ADD PRIMARY KEY (`id_supplier`);
 
 --
--- Indexes for table `transaksi_pelanggan`
+-- Indeks untuk tabel `transaksi_pelanggan`
 --
 ALTER TABLE `transaksi_pelanggan`
   ADD PRIMARY KEY (`id_transaksi`),
   ADD KEY `id_pelanggan` (`id_pelanggan`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT untuk tabel `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id_admins` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `barang`
+-- AUTO_INCREMENT untuk tabel `barang`
 --
 ALTER TABLE `barang`
   MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `karyawan`
+-- AUTO_INCREMENT untuk tabel `karyawan`
 --
 ALTER TABLE `karyawan`
   MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `keuangan`
+-- AUTO_INCREMENT untuk tabel `keuangan`
 --
 ALTER TABLE `keuangan`
-  MODIFY `id_keuangan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_keuangan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
--- AUTO_INCREMENT for table `pelanggan`
+-- AUTO_INCREMENT untuk tabel `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `supplier`
+-- AUTO_INCREMENT untuk tabel `supplier`
 --
 ALTER TABLE `supplier`
   MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `transaksi_pelanggan`
+-- AUTO_INCREMENT untuk tabel `transaksi_pelanggan`
 --
 ALTER TABLE `transaksi_pelanggan`
   MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `transaksi_pelanggan`
+-- Ketidakleluasaan untuk tabel `transaksi_pelanggan`
 --
 ALTER TABLE `transaksi_pelanggan`
   ADD CONSTRAINT `transaksi_pelanggan_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON DELETE CASCADE ON UPDATE CASCADE;
